416c0d5701a5f9ea00c065b8119898f389d1f8f4
MichalMahel <114081418+MichalMahel@users.noreply.github.com>
2022-12-08 18:57:19 +0100

    Add files via upload

--- Prevodnik_soustav/Prevodnik_soustav/Form1.Designer.cs
+++ Prevodnik_soustav/Prevodnik_soustav/Form1.Designer.cs
@@ -31,7 +31,6 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -40,7 +39,9 @@
             this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -51,11 +52,12 @@
             "2",
             "10",
             "16"});
-            this.comboBox1.Location = new System.Drawing.Point(70, 38);
+            this.comboBox1.Location = new System.Drawing.Point(499, 36);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(150, 21);
+            this.comboBox1.Size = new System.Drawing.Size(155, 24);
             this.comboBox1.TabIndex = 0;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
             // 
             // comboBox2
             // 
@@ -65,110 +67,136 @@
             "2",
             "10",
             "16"});
-            this.comboBox2.Location = new System.Drawing.Point(316, 38);
+            this.comboBox2.Location = new System.Drawing.Point(836, 36);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(150, 21);
+            this.comboBox2.Size = new System.Drawing.Size(141, 24);
             this.comboBox2.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(116, 158);
+            this.button1.Location = new System.Drawing.Point(349, 175);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(315, 36);
+            this.button1.Size = new System.Drawing.Size(420, 44);
             this.button1.TabIndex = 2;
             this.button1.Text = "Převeď";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(72, 106);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(395, 20);
-            this.textBox1.TabIndex = 3;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(72, 238);
+            this.textBox2.Location = new System.Drawing.Point(45, 266);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(395, 20);
+            this.textBox2.Size = new System.Drawing.Size(932, 22);
             this.textBox2.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(47, 41);
+            this.label1.Location = new System.Drawing.Point(468, 39);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.Size = new System.Drawing.Size(21, 17);
             this.label1.TabIndex = 5;
             this.label1.Text = "Z:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(284, 41);
+            this.label2.Location = new System.Drawing.Point(793, 38);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.Size = new System.Drawing.Size(33, 17);
             this.label2.TabIndex = 6;
             this.label2.Text = "DO:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(68, 90);
+            this.label3.Location = new System.Drawing.Point(43, 85);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(250, 13);
+            this.label3.Size = new System.Drawing.Size(205, 17);
             this.label3.TabIndex = 7;
-            this.label3.Text = "Zadej IPA v desítkové soustavě (192.168.130.240)\r\n";
+            this.label3.Text = "Zadej IPA v desítkové soustavě\r\n";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(212, 222);
+            this.label4.Location = new System.Drawing.Point(495, 246);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(98, 13);
+            this.label4.Size = new System.Drawing.Size(130, 17);
             this.label4.TabIndex = 8;
             this.label4.Text = "Výsledná IP adresa";
             // 
             // maskedTextBox1
             // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(73, 106);
-            this.maskedTextBox1.Mask = "00000000.00000000.00000000.00000000";
+            this.maskedTextBox1.Location = new System.Drawing.Point(45, 106);
+            this.maskedTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(394, 20);
+            this.maskedTextBox1.Size = new System.Drawing.Size(932, 22);
             this.maskedTextBox1.TabIndex = 9;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(71, 90);
+            this.label5.Location = new System.Drawing.Point(43, 85);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(360, 13);
+            this.label5.Size = new System.Drawing.Size(201, 17);
             this.label5.TabIndex = 10;
-            this.label5.Text = "Zadej IPA v dvojkové soustavě (11000111 01100101 01101001 1101001)";
+            this.label5.Text = "Zadej IPA v dvojkové soustavě";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(70, 90);
+            this.label6.Location = new System.Drawing.Point(43, 85);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(232, 13);
+            this.label6.Size = new System.Drawing.Size(228, 17);
             this.label6.TabIndex = 12;
-            this.label6.Text = "Zadej IPA v hexadecimální soustavě (ff.f5.a3.9)\r\n";
+            this.label6.Text = "Zadej IPA v šestnáctkové soustavě\r\n";
             // 
-            // textBox3
+            // comboBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(71, 106);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(395, 20);
-            this.textBox3.TabIndex = 13;
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "IPv4",
+            "IPv6"});
+            this.comboBox3.Location = new System.Drawing.Point(156, 33);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(119, 24);
+            this.comboBox3.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(43, 36);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(106, 17);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "výběr IP adresy";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(45, 162);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 16;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(526, 314);
-            this.Controls.Add(this.textBox3);
+            this.ClientSize = new System.Drawing.Size(1024, 341);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox3);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.maskedTextBox1);
@@ -177,10 +205,10 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -194,7 +222,6 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
@@ -203,7 +230,9 @@
         private System.Windows.Forms.MaskedTextBox maskedTextBox1;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Prevodnik_soustav/Prevodnik_soustav/Form1.cs
+++ Prevodnik_soustav/Prevodnik_soustav/Form1.cs
@@ -20,143 +20,185 @@ namespace Prevodnik_soustav
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            maskedTextBox1.Visible = false;
+            //textBox1.Visible = false;
             label3.Visible = false;
-            textBox1.Visible = false;
             label5.Visible = false;
-            textBox3.Visible = false;
             label6.Visible = false;
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            switch (comboBox1.SelectedIndex)
+            switch (comboBox3.SelectedIndex)
             {
                 case 0:
-                    ;
-                    maskedTextBox1.Visible = true;
-                    label5.Visible = true;
-                    textBox1.Visible = false;
-                    label3.Visible = false;
-                    textBox3.Visible = false;
-                    label6.Visible = false;
-                    break;
-                case 1:
-                    maskedTextBox1.Visible = false;
-                    label5.Visible = false;
-                    textBox1.Visible = true;
-                    label3.Visible = true;
-                    textBox3.Visible = false;
-                    label6.Visible = false;
-                    break;
-                case 2:
-                    maskedTextBox1.Visible = false;
-                    label5.Visible = false;
-                    textBox1.Visible = false;
-                    label3.Visible = false;
-                    textBox3.Visible = true;
-                    label6.Visible = true;
-                    break;
-            }
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-            switch (comboBox1.SelectedIndex)
-            {
-                case 0:
-                    switch (comboBox2.SelectedIndex)
+                    switch (comboBox1.SelectedIndex)
                     {
-                        //hotovo
                         case 0:
-                            string cislo1 = maskedTextBox1.Text;
-                            textBox2.Text = cislo1.ToString();
+                            label5.Visible = true;
+                            label3.Visible = false;
+                            label6.Visible = false;
+                            maskedTextBox1.Mask = "00000000.00000000.00000000.00000000";
                             break;
-                        
                         case 1:
-                            string cislo2 = textBox1.Text;
-                            int Vysledne_cislo = 0;
-                            double delka = cislo2.Length;
-                            double mocnina = 0;
-                            foreach (char c in cislo2)
-                            {
-                                if (c == '1')
-                                {
-                                    mocnina = Math.Pow(2, delka - 1);
-                                    Vysledne_cislo += (int)mocnina;
-                                    mocnina = 0;
-                                    delka--;
-                                }
-                                else
-                                {
-                                    delka--;
-                                }
-                            }
-                        //string cislo2 = textBox1.Text;
-                        //string Vysledne_cislo = Convert.ToString(Convert.ToInt32(cislo2, 2), 10);
-                        //textBox2.Text = Vysledne_cislo.ToString();
-                        break;
-
+                            label5.Visible = false;
+                            label3.Visible = true;
+                            label6.Visible = false;
+                            maskedTextBox1.Mask = "000.000.000.000";
+                            break;
                         case 2:
-                            string cislo3 = textBox1.Text;
-                            string hex = Convert.ToString(Convert.ToInt32(cislo3, 2), 16);
-                            textBox2.Text = hex.ToString();
+                            label5.Visible = false;
+                            label3.Visible = false;
+                            label6.Visible = true;
+                            maskedTextBox1.Mask = "CC.CC.CC.CC";
                             break;
                     }
-                   ; break;
+                    break;
                 case 1:
-                    switch (comboBox2.SelectedIndex)
+                    switch (comboBox1.SelectedIndex)
                     {
-                        
                         case 0:
-                            //string cislo3 = textBox1.Text;
-                            //string Vysledne_cislo = Convert.ToString(Convert.ToInt32(cislo3, 10), 2);
-                            //textBox2.Text = Vysledne_cislo.ToString(); break;
-                            textBox2.Text = "";
-                            int cislo1 = Convert.ToInt32(textBox1.Text);
-                            List<int> list1 = new List<int>();
-                            int vysledek = 0;
-
-                            while (cislo1 >= 1)
-                            {
-                                vysledek = cislo1 % 2;
-                                cislo1 = cislo1 / 2;
-                                list1.Add(vysledek);
-                            }
-                            while (list1.Count % 4 != 0)
-                            {
-                                list1.Add(0);
-                            }
-                            list1.Reverse();
-                            foreach (int s in list1)
-                            {
-                                textBox2.Text += s.ToString();
-                            }
+                            label5.Visible = true;
+                            label3.Visible = false;
+                            label6.Visible = false;
+                            maskedTextBox1.Mask = "000000000000:000000000000:000000000000:000000000000:000000000000:000000000000";
                             break;
-                        //hotovo
                         case 1:
-                            string cislo2 = textBox1.Text;
-                            textBox2.Text = cislo2.ToString(); break;
-                        case 2:; break;
-                    }
-                   ; break;
-                case 2:
-                    switch (comboBox2.SelectedIndex)
-                    {
-                        case 0:
-                            //string bc = Convert.ToString(Convert.ToInt32(hc, 16), 2);
-
-
+                            label5.Visible = false;
+                            label3.Visible = true;
+                            label6.Visible = false;
+                            maskedTextBox1.Mask = "00000:00000:00000:00000:00000:00000";
                             break;
-                        case 1:; break;
-                        case 2:; break;
+                        case 2:
+                            label5.Visible = false;
+                            label3.Visible = false;
+                            label6.Visible = true;
+                            maskedTextBox1.Mask = "CCCC:CCCC:CCCC:CCCC:CCCC:CCCC";
+                            break;
                     }
-                   ; break;
+                    break;
             }
         }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            int Z = 0;
+            int DO = 0;
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    Z = 2;
+                    switch (comboBox2.SelectedIndex)
+                    {
+                        case 0:
+                            DO = 2;break;
+                        case 1:
+                            DO = 10;break;
+                        case 2:
+                            DO = 16;break;
+                    }
+                    break;
+                case 1:
+                    Z = 10;
+                    switch (comboBox2.SelectedIndex)
+                    {
+                        case 0:
+                            DO = 2; break;
+                        case 1:
+                            DO = 10; break;
+                        case 2:
+                            DO = 16; break;
+                    }
+                    break;
+                case 2:
+                    Z = 16;
+                    switch (comboBox2.SelectedIndex)
+                    {
+                        case 0:
+                            DO = 2; break;
+                        case 1:
+                            DO = 10; break;
+                        case 2:
+                            DO = 16; break;
+                    }
+                    break;
+            }
+            string cislo = maskedTextBox1.Text;
+            int Pomocna_promena_pro_osetreni_cisel = 0;
+            List<string> list = new List<string>();
+            if (maskedTextBox1.Text.Contains(","))
+            {
+                string[] pole = cislo.Split(',');
+                foreach (string cis in pole)
+                {
+                    string vys = Convert.ToString(Convert.ToInt32(cis, Z), DO);
+                    list.Add(vys.ToString());
+                }
 
-        
+                // Nefunguje mi to a nevim proč
+                //foreach (string cis in list)
+                //{
+                //    string vys1 = Convert.ToString(Convert.ToInt32(cis, Z), 10);
+                //    textBox1.Text = vys1.ToString();
+                //    int vys = Convert.ToInt32(textBox1.Text);
+                //    if (vys > 255 || vys < 0)
+                //    {
+                //        MessageBox.Show("Blbe");
+                //        Severní_vitr++;
+                //    }
+                //}
+            }
+            if (maskedTextBox1.Text.Contains(":"))
+            {
+                string[] pole = cislo.Split(':');
+                foreach (string cis in pole)
+                {
+                    string vys = Convert.ToString(Convert.ToInt32(cis, Z), DO);
+                    list.Add(vys.ToString());
+                }
+
+                // Nefunguje mi to a nevim proč
+                //foreach (string cis in list)
+                //{
+                //    string vys1 = Convert.ToString(Convert.ToInt32(cis, Z), 10);
+                //    textBox1.Text = vys1.ToString();
+                //    int vys = Convert.ToInt32(textBox1.Text);
+                //    if (vys > 65535 || vys < 0)
+                //    {
+                //        MessageBox.Show("Vole");
+                //        Severní_vitr++;
+                //    }
+                //}
+            }
+
+            if (Pomocna_promena_pro_osetreni_cisel == 0)
+            {
+                int pom = 1;
+                foreach (string cis in list)
+                {
+                    textBox2.Text += cis.ToString();
+                    if (maskedTextBox1.Text.Contains(","))
+                    {
+                        if (pom != list.Count)
+                        {
+                            textBox2.Text += ".";
+                        }
+                    }
+                    if (maskedTextBox1.Text.Contains(":"))
+                    {
+                        if (pom != list.Count)
+                        {
+                            textBox2.Text += ":";
+                        }
+                    }
+                    pom++;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Zadal jsi špatně prvotní IP adresu! Zkus to znovu.");
+            }
+
+        }
     }
     
 }
